a10e49eba5f38cfec88ff69597bcf95587b44fb8
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2022-12-04 23:41:21 +0300

    Using insertAdj

--- UI/JS/script.js
+++ UI/JS/script.js
@@ -3,6 +3,7 @@ function addTask() {
   const task = document.createElement("button");
   task.className = "my-1 rounded-md bg-zinc-800 p-2 pr-36 text-left";
   task.innerHTML = inputValue.value;
-  document.getElementById("taskList").prepend(task);
+  let itemGet = document.getElementById("taskList");
+  itemGet.insertAdjacentElement("afterbegin", task);
   inputValue.value = "";
 }
