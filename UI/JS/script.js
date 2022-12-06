1778ef49691d2a1e9f587badc1c83a550b8bea5c
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2022-12-06 06:35:16 +0300

    delete button + double click - event listener

--- UI/JS/script.js
+++ UI/JS/script.js
@@ -1,9 +1,28 @@
 function addTask() {
-  let inputValue = document.getElementById("backlog");
-  const task = document.createElement("button");
-  task.className = "my-1 rounded-md bg-zinc-800 p-2 pr-36 text-left";
-  task.innerHTML = inputValue.value;
-  let itemGet = document.getElementById("taskList");
-  itemGet.insertAdjacentElement("afterbegin", task);
+  let inputValue = document.getElementById("submitButton");
+  let taskCell = document.createElement("div");
+  taskCell.className =
+    "my-1 rounded-md bg-zinc-800 p-2 text-left flex items-center justify-between max-w-sm ";
+
+  let taskTitle = document.createElement("button");
+  taskTitle.className = "break-all ";
+  taskTitle.innerHTML = inputValue.value;
+
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "text-red-500  rounded-md  mx-4 hover:text-red-600 ";
+  deleteButton.innerHTML = "Delete";
+
+  taskCell.append(taskTitle, deleteButton);
   inputValue.value = "";
+
+  document.getElementById("backLog").prepend(taskCell);
+
+  deleteButton.addEventListener("click", function () {
+    taskCell.remove();
+    deleteButton.remove();
+  });
+
+  taskCell.addEventListener("dblclick", function () {
+    document.getElementById("inProgress").prepend(taskCell);
+  });
 }
